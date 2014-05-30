d5201c20758e16abb02a404e35aa2d4514218ac2
Dan Wagner <daniel.wagner@linkshare.com>
2014-05-30 11:08:12 -0500

    first check-in, initial spec

---
+++ README.md
@@ -0,0 +1,4 @@
+Flix
+====
+
+"Ask me what 9 times F is. It's fleventy-five!" -Erlich Bachman
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Flix::Application.config.secret_key_base = 'cec8fe795e7496038c626e5da1e34437c29df14e4e41b3f7677e9a51d0ef0b8b0a372cf2e4797306195636a03cb1db723cdd0b0c9abb40f7e85f3f6b84fc7d3d'
