d33d27ccb4e387306da446c0d70105f1f111958c
Dan Wagner <daniel.wagner@linkshare.com>
2014-06-20 10:19:49 -0500

    work through forms editing (part 2)

--- spec/features/edit_movie_spec.rb
+++ spec/features/edit_movie_spec.rb
@@ -6,8 +6,17 @@ describe 'edit a movie' do
     movie = Movie.create(movie_attributes)
     visit movie_url(movie)
     click_link 'Edit'
+
+    # check that links work
     expect(current_path).to eql(edit_movie_path(movie))
     expect(find_field('Title').value).to eql(movie.title)
+
+    fill_in 'Title', with: "Updated Movie Title"
+    click_button 'Update Movie'
+    
+    # check that update works
+    expect(current_path).to eq(movie_path(movie))
+    expect(page).to have_text('Updated Movie Title')
   end
 
 end
