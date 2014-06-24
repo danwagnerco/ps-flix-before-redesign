3e0dee862b07c20e7ea460d5760f33c5e80a6097
Dan Wagner <daniel.wagner@linkshare.com>
2014-06-24 15:54:03 -0500

    work through custom queries

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,4 +2,23 @@ class Movie < ActiveRecord::Base
   def flop?
     total_gross.blank? || total_gross < 50_000_000
   end
+
+  def self.released
+    where("released_on <= ?", Time.now).order(released_on: :desc)
+  end
+
+  def self.hit
+    hit_gross = 300_000_000
+    where("total_gross >= ?", hit_gross).order(total_gross: :desc)
+  end
+
+  def self.flop
+    flop_gross = 50_000_000
+    where("total_gross <= ?", flop_gross).order(total_gross: :asc)
+  end
+
+  def self.recently_added
+    # only want the three movies with the latest created_on dates
+    order(created_at: :desc).limit(3)
+  end
 end
--- spec/models/movie_spec.rb
+++ spec/models/movie_spec.rb
@@ -14,4 +14,36 @@ describe 'A movie' do
     movie = Movie.new(total_gross: 500_000_000.00)
     expect(movie.flop?).to eql(false)
   end
+  it "is released when the released on date is in the past" do
+  movie = Movie.create(movie_attributes(released_on: 3.months.ago))
+ 
+  expect(Movie.released).to include(movie)
+  end
+
+  it "is not released when the released on date is in the future" do
+    movie = Movie.create(movie_attributes(released_on: 3.months.from_now))
+    expect(Movie.released).not_to include(movie)
+  end
+
+  it "returns released movies ordered with the most recently-released movie first" do
+    movie1 = Movie.create(movie_attributes(released_on: 3.months.ago))
+    movie2 = Movie.create(movie_attributes(released_on: 2.months.ago))
+    movie3 = Movie.create(movie_attributes(released_on: 1.months.ago))
+    expect(Movie.released).to eq([movie3, movie2, movie1])
+  end
+
+  it "returns hit movies (+300MM) ordered with the highest grossing movie first" do
+    movie1 = Movie.create(movie_attributes(total_gross: 500_000_000))
+    movie2 = Movie.create(movie_attributes(total_gross: 400_000_000))
+    movie3 = Movie.create(movie_attributes(total_gross: 300_000_000))
+    expect(Movie.hit).to eq([movie1, movie2, movie3])
+  end
+
+  it "returns flop movies (-50MM) ordered with the lowest grossing movie first" do
+    movie1 = Movie.create(movie_attributes(total_gross: 49_000_000))
+    movie2 = Movie.create(movie_attributes(total_gross: 48_000_000))
+    movie3 = Movie.create(movie_attributes(total_gross: 47_000_000))
+    expect(Movie.flop).to eq([movie3, movie2, movie1])
+  end
+
 end
